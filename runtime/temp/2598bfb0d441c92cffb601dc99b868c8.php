fb9de6ce6c77d286d8211d88db2498ed86809119
sunaiwen <1872937640@qq.com>
2017-07-17 20:59:26 +0800

    推送本地blog到远程

---
+++ runtime/temp/2598bfb0d441c92cffb601dc99b868c8.php
@@ -0,0 +1,127 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:2:{s:62:"D:\phpStudy\WWW\blog\public/../app/index\view\index\index.html";i:1500282648;s:64:"D:\phpStudy\WWW\blog\public/../app/index\view\public\footer.html";i:1500281710;}*/ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>苏乃文的博客</title>
+	<style>
+		.all{
+			position:relative;
+			width:100%;
+			height: 1000px;
+			left:opx;
+			top:0px;
+			background: gray;
+		}
+
+		.index{
+			position:relative;
+			width:200px;
+			height: 500px;
+			left:25px;
+			top:0px;
+			background: gray;
+		}
+
+		.user{
+			width: 200px;
+			height: 40px;
+			position: absolute;
+			border: 1px solid #f7f7f7;
+			background: #f7f7f7;
+			box-shadow: 2px 0 5px rgba(0,0,0,.3);
+			left:0px;
+			top: 0;
+			box-sizing: border-box;
+			margin: 0;
+			padding: 10px;
+			background: gray;
+			font-size: 20px;
+			cursor: pointer;
+		}
+
+
+		.tit{
+			width: 200px;
+			position: absolute;
+			border: 1px solid #f7f7f7;
+			background: #f7f7f7;
+			box-shadow: 2px 0 5px rgba(0,0,0,.3);
+			left:0px;
+			top: 40px;
+			box-sizing: border-box;
+			margin: 0;
+			padding: 10px;
+			background: gray;
+		}
+
+		li{
+			display: block;
+			height:30px;
+			line-height: 30px;
+			padding-left: 12px;
+			cursor: pointer;
+			font-size: 18px;
+			position:relative;
+		}
+
+		.con{
+			width: 1000px;
+			position: absolute;
+			border: 1px solid #f7f7f7;
+			background: #f7f7f7;
+			box-shadow: 2px 0 5px rgba(0,0,0,.3);
+			left:200px;
+			top: 0px;
+			box-sizing: border-box;
+			margin: 0;
+			padding: 10px;
+			background: gray;
+		}
+
+		.footer{
+			width:1200px;
+			position: absolute;
+			left:0px;
+			top:900px;
+			text-align: center;
+		}
+
+
+
+	</style>
+</head>
+<body>
+<div class="all">
+	<DIV class="index">
+
+		<div class="user">
+		苏乃文的博客
+		</div>
+
+		<div class="tit">
+			<ul>
+				<li>首页</li>
+				<li>标签</li>
+				<li>关于</li>
+			</ul>
+		</div>
+		<div class="con">
+				neirong<br>
+				neirong<br>
+				neirong<br>
+				neirong<br>
+				neirong<br>
+				neirong<br>
+				neirong<br>
+
+		</div>
+
+
+
+	</DIV>
+					<div class="footer">
+						yejiao 
+				</div>
+</div>
+</body>
+</html>
